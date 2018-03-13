6e9b8b125693a8af52088c745f273d0888c4e91c
Piotrek Kochański <mailpiotrk@gmail.com>
2018-03-13 14:29:42 +0100

    add sample code to main file

---
+++ src/main.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { CASTLE, ROAD, FIELD } element;
+typedef struct tile {
+    element up;
+    element right;
+    element down;
+    element left;
+    bool temple;
+};
+
+typedef enum { INTERACTIVE, AUTO } gamemode;
+
+void greeting(void) {
+    printf("hello player!\n"
+           "welcome to carcassonne game!\n"
+           "something something\n");
+}
+
+int main(int argc, char* argv[]) {
+    gamemode mode = AUTO;
+    if (argc < 2) {
+        mode = INTERACTIVE;
+        greeting();
+    }
+
+    return EXIT_SUCCESS;
+}
