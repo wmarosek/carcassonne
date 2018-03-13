8845a7f99f6948cfe09e041e9b833124e3776221
Piotrek Kochański <mailpiotrk@gmail.com>
2018-03-13 16:46:20 +0100

    fix typedefing struct, add function to rotate tiles

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 
 typedef enum { CASTLE, ROAD, FIELD } element;
-typedef struct tile {
+typedef struct {
     element up;
     element right;
     element down;
     element left;
     bool shield;
     bool temple;
-};
+} tile;
 
 typedef enum { INTERACTIVE, AUTO } gamemode;
 
@@ -18,6 +18,18 @@ void greeting(void) {
     printf("hello player!\n"
            "welcome to carcassonne game!\n"
            "something something\n");
+}
+
+void rotate_tile(tile* t) {
+    element
+        u = t->up,
+        r = t->right,
+        d = t->down,
+        l = t->left;
+    t->up = r;
+    t->right = d;
+    t->down = l;
+    t->left = u;
 }
 
 int main(int argc, char* argv[]) {
