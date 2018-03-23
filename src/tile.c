262bcc1045153536bd5f6b9e2363de31ad73ceb7
Piotrek Kochański <mailpiotrk@gmail.com>
2018-03-23 14:57:34 +0100

    move function definition to cpp file to avoid redefinitions

--- src/tile.c
+++ src/tile.c
@@ -1,1 +1,13 @@
 #include "tile.h"
+
+void rotate_tile(tile* t) {
+    element
+        u = t->up,
+        r = t->right,
+        d = t->down,
+        l = t->left;
+    t->up = r;
+    t->right = d;
+    t->down = l;
+    t->left = u;
+}
--- src/tile.h
+++ src/tile.h
@@ -14,16 +14,6 @@ typedef struct {
     bool temple;
 } tile;
 
-void rotate_tile(tile* t) {
-    element
-        u = t->up,
-        r = t->right,
-        d = t->down,
-        l = t->left;
-    t->up = r;
-    t->right = d;
-    t->down = l;
-    t->left = u;
-}
+void rotate_tile(tile* t);
 
 #endif
