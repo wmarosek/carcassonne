6915f924b5abecb5abaa1ae868ec80586a8ee437
Piotrek Kochański <mailpiotrk@gmail.com>
2018-04-06 16:27:32 +0200

    remove global board declaration, add can_place_tile funciton

--- src/board.c
+++ src/board.c
@@ -1,6 +1,48 @@
 #include "board.h"
 
-// should this be array of pointers or array of structs,
-// with pointers we could represent empty space by null pointer,
-// with structs we would probably need additional switch specifing uninitalized state
-tile* board_g[BOARD_SIZE][BOARD_SIZE] = {{ 0 }};
+bool can_place_tile(size_t size, tile* board[size][size],
+                    tile* t, size_t height, size_t width) {
+    // if out of bounds return false
+    if (height > size || width > size) {
+        return false;
+    }
+    // return false if target cell is already populated
+    if (board[height][width]) {
+        return false;
+    }
+    // count amount of surrounding tiles
+    int count = 0;
+    // check if placement is on the edge and if neighbour tile is not null
+    if (height != 0 && board[height - 1][width]) {
+        // increase amount of surrounding tiles count
+        ++count;
+        // check if the types are the same
+        if (board[height - 1][width]->down != t->up) {
+            return false;
+        }
+    }
+    if (height != size && board[height + 1][width]) {
+        ++count;
+        if (board[height + 1][width]->up != t->down) {
+            return false;
+        }
+    }
+    if (width != 0 && board[height][width - 1]) {
+        ++count;
+        if (board[height][width - 1]->right != t->left) {
+            return false;
+        }
+    }
+    if (width != size && board[height][width + 1]) {
+        ++count;
+        if (board[height][width + 1]->left != t->right) {
+            return false;
+        }
+    }
+    // return false if no surrounding tiles
+    return count > 0;
+}
+
+void place_tile(tile** place, tile* t) {
+    *place = t;
+}
