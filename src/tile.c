cb275d67ed964809def803452ddc14b0330f2b48
Piotrek Kochański <mailpiotrk@gmail.com>
2018-04-07 22:26:08 +0200

    use putchar for printing newline instead of printf

--- src/tile.c
+++ src/tile.c
@@ -142,7 +142,7 @@ void print_tile(const tile* t) {
 void print_tile_list(const tile* t, size_t len) {
     for (unsigned int i = 0; i < len; ++i) {
         print_tile(&t[i]);
-        printf("\n");
+        putchar('\n');
     }
 }
 
