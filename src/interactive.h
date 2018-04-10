6c74fd42c6b3373a2396874973d85eb24b197dea
Piotrek Kochański <mailpiotrk@gmail.com>
2018-04-10 17:52:09 +0200

    help and run_interactive documentation

--- src/interactive.h
+++ src/interactive.h
@@ -12,7 +12,16 @@ void usage();
  */
 void greeting();
 
+/**
+ * print interactive help
+ */
 void help();
-void run_interactive();
+
+/**
+ * interactive mode logic and game loop.
+ * @param tile_list tile list
+ * @param list_len list length
+ */
+void run_interactive(tile_list_t*, size_t);
 
 #endif
