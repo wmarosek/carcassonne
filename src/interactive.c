a49bdde2cf7e4c3ed74330b5011742a4e43515e3
Piotrek Kochański <mailpiotrk@gmail.com>
2018-04-21 19:54:58 +0200

    add function and action for choosing tile, action for printing valid moves

--- src/interactive.c
+++ src/interactive.c
@@ -55,6 +55,40 @@ void load_board_interactive(sized_board* board) {
     }
 }
 
+tile* choose_tile_interactive(sized_tlist* list, tile** t) {
+    tile* temp = *t;
+    fputs("choose tile (number): ", stdout);
+    size_t choice;
+    while (true) {
+        scanf("%lu", &choice);
+        // exhaust stdin
+        for (int ch; (ch = getchar()) != EOF && ch != '\n' && ch != '\r';) { ; }
+        if (choice == 0 || choice <= list->len) {
+            break;
+        }
+        puts("choice out of bounds");
+    }
+    // choose right tile based on user input (numbering from 1 and ignore null pointers)
+    for (size_t i = 0, count = 0; i < list->len; ++i) {
+        if (list->tiles[i] && ++count == choice) {
+            *t = list->tiles[i];
+            list->tiles[i] = 0;
+        }
+    }
+    // if current tile is not null put it back on the list
+    if (temp) {
+        // find empty space
+        for (size_t i = 0; i < list->len; ++i) {
+            // TODO: if it wont find empty space it will leak memory
+            if (list->tiles[i] == 0) {
+                list->tiles[i] = temp;
+                break;
+            }
+        }
+    }
+    return temp;
+}
+
 typedef enum {
     ACT_GREETING,
     ACT_USAGE,
@@ -63,6 +97,8 @@ typedef enum {
     ACT_LOAD_LIST,
     ACT_PRINT_BOARD,
     ACT_LOAD_BOARD,
+    ACT_CHOOSE_TILE,
+    ACT_PRINT_MOVES,
     ACT_CHNG_PRMPT,
     ACT_QUIT,
     ACT_UNKNOWN,
@@ -73,24 +109,52 @@ typedef enum {
 // put them right after main command
 // (only first command with specific enum value is printed)
 const struct { action act; const char* cmd; const char* desc; } act_list[] = {
-    { ACT_GREETING,     "greeting",     "greets player"         },
-    { ACT_GREETING,     "g",            "abbrev"                },
-    { ACT_USAGE,        "usage",        "prints usage"          },
-    { ACT_USAGE,        "u",            "abbrev"                },
-    { ACT_HELP,         "help",         "prints this message"   },
-    { ACT_HELP,         "h",            "abbrev"                },
-    { ACT_HELP,         "?",            "abbrev"                },
-    { ACT_PRINT_LIST,   "print list",   "prints tile list"      },
-    { ACT_PRINT_LIST,   "p l",          "abbrev"                },
-    { ACT_LOAD_LIST,    "load list",    "load tile list file"   },
-    { ACT_LOAD_LIST,    "l l",          "abbrev"                },
-    { ACT_PRINT_BOARD,  "print board",  "prints the board"      },
-    { ACT_PRINT_BOARD,  "p b",          "abbrev"                },
-    { ACT_LOAD_BOARD,   "load board",   "load board file"       },
-    { ACT_LOAD_BOARD,   "l b",          "abbrev"                },
-    { ACT_CHNG_PRMPT,   "prompt",       "change prompt text"    },
-    { ACT_QUIT,         "quit",         "quits the game"        },
-    { ACT_QUIT,         "q",            "abbrev"                },
+    // greeting
+    { ACT_GREETING,     "greeting",     "greets player"                     },
+    { ACT_GREETING,     "g",            "abbrev"                            },
+
+    // usage
+    { ACT_USAGE,        "usage",        "prints usage"                      },
+    { ACT_USAGE,        "u",            "abbrev"                            },
+
+    // help
+    { ACT_HELP,         "help",         "prints this message"               },
+    { ACT_HELP,         "h",            "abbrev"                            },
+    { ACT_HELP,         "?",            "abbrev"                            },
+
+    // list printing
+    { ACT_PRINT_LIST,   "print list",   "prints tile list"                  },
+    { ACT_PRINT_LIST,   "p l",          "abbrev"                            },
+
+    // loading list file
+    { ACT_LOAD_LIST,    "load list",    "load tile list file"               },
+    { ACT_LOAD_LIST,    "l l",          "abbrev"                            },
+
+    // board printing
+    { ACT_PRINT_BOARD,  "print board",  "prints the board"                  },
+    { ACT_PRINT_BOARD,  "p b",          "abbrev"                            },
+
+    // loading board file
+    { ACT_LOAD_BOARD,   "load board",   "load board file"                   },
+    { ACT_LOAD_BOARD,   "l b",          "abbrev"                            },
+
+    // choosing tile to place
+    { ACT_CHOOSE_TILE,  "choose tile",  "choose tile to place"              },
+    { ACT_CHOOSE_TILE,  "c t",          "abbrev"                            },
+
+    // print aviable moves with current tile
+    { ACT_PRINT_MOVES,  "print moves",  "print moves aviable"
+                                        " with current tile"                },
+    { ACT_PRINT_MOVES,  "p m",          "abbrev"                            },
+
+    // chnaging prompt text
+    { ACT_CHNG_PRMPT,   "prompt",       "change prompt text"                },
+
+    // quiting
+    { ACT_QUIT,         "quit",         "quits the game"                    },
+    { ACT_QUIT,         "q",            "abbrev"                            },
+    { ACT_QUIT,         "exit",         "abbrev"                            },
+    { ACT_QUIT,         "e",            "abbrev"                            },
 };
 
 void help() {
@@ -139,6 +203,8 @@ void run_interactive(gamemode mode, const char* list_filename) {
         load_board_interactive(&board);
     }
 
+    tile* curr_tile = 0;
+
     while (true) {
         switch(handle_input()) {
         case ACT_GREETING:
@@ -162,12 +228,20 @@ void run_interactive(gamemode mode, const char* list_filename) {
         case ACT_LOAD_BOARD:
             load_board_interactive(&board);
             break;
+        case ACT_CHOOSE_TILE:
+            choose_tile_interactive(&list, &curr_tile);
+            break;
+        case ACT_PRINT_MOVES:
+            print_board_legal_moves(&board, curr_tile);
+            break;
         case ACT_CHNG_PRMPT:
             change_prompt();
             break;
         case ACT_QUIT:
             tlist_free(&list);
             board_free(&board);
+            tile_free(curr_tile);
+            free(curr_tile);
             return;
         default: fputs("unknown option\n", stderr);
         }
