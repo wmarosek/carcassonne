7109b39d10568cf663fb74fd07a9bba12d2bf4fc
Piotrek Kochański <mailpiotrk@gmail.com>
2018-06-06 15:25:32 +0200

    fix spacing, unnecesary heap allocation, unused return value

--- src/generator.c
+++ src/generator.c
@@ -1,36 +1,36 @@
+#include "tlist.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
 
-#include "tlist.h"
-
-bool tileGenerator(const char* default_tiles,const char* new_tiles, int num);
+void tileGenerator(const char* default_tiles,const char* new_tiles, int num);
 
 int main(int argc, char* argv[]) {
     if(argc != 4) {
-        puts("Wrong input!\nFormat: [DEFAULT_TILES_FILE] [NEW_TILES_FILE] [NUM_OF_NEW_TILES]");
+        puts("Wrong input!\n"
+             "Format: [DEFAULT_TILES_FILE] [NEW_TILES_FILE] [NUM_OF_NEW_TILES]");
+        return EXIT_FAILURE;
     }
 
-    tileGenerator(argv[1],argv[2],atoi(argv[3]));
-    return 0;
+    tileGenerator(argv[1], argv[2], atoi(argv[3]));
+    return EXIT_SUCCESS;
 }
 
-bool tileGenerator(const char* default_tiles,const char* new_tiles, int num) {
+void tileGenerator(const char* default_tiles,const char* new_tiles, int num) {
     sized_tlist default_list = tlist_init_exit_on_err(default_tiles);
     
-    sized_tlist* new_list = malloc(sizeof(sized_tlist));
-    new_list->size = num;
-    new_list->tiles = malloc(num*sizeof(tile*));
+    sized_tlist new_list;
+    new_list.size = num;
+    new_list.tiles = malloc(num * sizeof(tile*));
 
     srand(time(NULL));
     for(int i = 0; i < num; i++) {
-        new_list->tiles[i] = default_list.tiles[rand()%default_list.size];
+        new_list.tiles[i] = default_list.tiles[rand() % default_list.size];
     }
 
-    tlist_write(new_list,new_tiles);
+    tlist_write(&new_list,new_tiles);
     tlist_free(&default_list);
-    free(new_list->tiles);
-    free(new_list);
-    return true;
+    tlist_free(&new_list);
 }
